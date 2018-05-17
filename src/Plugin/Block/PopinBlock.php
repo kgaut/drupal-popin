1c60b3b0fe46aa2d3cd2dc50de42e221eae1034b
Kevin Gautreau <contact@kgaut.net>
2018-05-17 11:14:12 +0200

    Add image style config + bloc

--- src/Plugin/Block/PopinBlock.php
+++ src/Plugin/Block/PopinBlock.php
@@ -3,6 +3,9 @@
 namespace Drupal\popin\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Image\Image;
+use Drupal\file\Entity\File;
+use Drupal\image\Entity\ImageStyle;
 
 /**
  * Provides a 'PopinBlock' block.
@@ -19,6 +22,15 @@ class PopinBlock extends BlockBase {
    */
   public function build() {
     $config = \Drupal::config('popin.popinconfig')->getRawData();
+    if($config['enabled'] !== 1) {
+      return [];
+    }
+    $image_style = ImageStyle::load('popin');
+    $image = File::load($config['image'][0]);
+    $config['image'] = $image_style->buildUrl($image->getFileUri());
+
+    dpm($config);
+
     $build = [];
     $build['popin_block']['#theme'] = 'popin_block';
 
